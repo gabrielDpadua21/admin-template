23e4c831cad4962f1f4b2cc09d01c6b89ab89025
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2024-02-08 01:49:55 -0300

    feat: auth page styles

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -11,18 +11,40 @@ const Auth = () => {
     const [confirmPassword, setConfirmPassword] = useState('');
     const [mode, setMode] = useState<'login' | 'register' | 'forgotPassword'>('login');
 
+    const send = () => {
+        console.log('send');
+    }
+
     return (
-        <div className={`flex flex-col`}>
-            <h1>Auth</h1>
-            <AuthInput label="E-mail" type="email" value={mail} changeValue={setMail} isRequired />
-            <AuthInput label="Password" type="password" value={password} changeValue={setPassword} isRequired />
-            <AuthInput 
-                label="ConfirmPassword"
-                type="password" 
-                value={confirmPassword} 
-                changeValue={setConfirmPassword} 
-                isRequired notRendered={mode == 'login'}
-            />
+        <div className={`w-screen h-screen flex items-center justify-center p-2 bg-cover bg-[url('https://source.unsplash.com/random')]`}>
+            <div className={`bg-gray-100 p-10 content-center rounded-md flex flex-col items-center w-screen md:w-1/3`}>
+                <h1 className={`text-xl font-bold mb-5`}>
+                    {mode === 'login' ? 'Faça Login na sua conta' : 'Cadastre-se na Plataforma'}
+                </h1>
+                <AuthInput label="E-mail" type="email" value={mail} changeValue={setMail} isRequired />
+                <AuthInput label="Password" type="password" value={password} changeValue={setPassword} isRequired />
+                <AuthInput 
+                    label="ConfirmPassword"
+                    type="password" 
+                    value={confirmPassword} 
+                    changeValue={setConfirmPassword} 
+                    isRequired notRendered={mode == 'login'}
+                />
+                <button className={`bg-indigo-600 hover:bg-indigo-300 px-4 py-3 rounded-md text-white w-full mt-5`} onClick={send}>
+                    {mode === 'login' ? 'Entrar' : 'Cadastrar'}
+                </button>
+                <hr className={`my-2 w-full`} />
+                <button className={`bg-red-600 hover:bg-red-300 px-4 py-3 rounded-md text-white w-full `} onClick={send}>
+                    Google Login
+                </button>
+
+                <p className={`mt-8`}>
+                    <a onClick={() => setMode( mode === 'login' ? 'register' : 'login')} className={`text-blue-500 hover:text-blue-700 font-semibold cursor-pointer`}>
+                        {mode === 'login' ? 'Não tem cadastro? Criar uma conta' : 'Já tenho uma conta! Fazer Login'}
+                    </a>
+                </p>
+
+            </div>
         </div>
     )
 }
--- src/components/auth/AuthInput.tsx
+++ src/components/auth/AuthInput.tsx
@@ -12,9 +12,15 @@ const AuthInput = (props: AuthInputProps) => {
     const { label, value, changeValue, isRequired, notRendered, type = 'text' } = props;
     
     return ( notRendered ? null : 
-        <div className={`flex flex-col`}>
-            <label>{label}</label>
-            <input type={type} value={value} onChange={e => changeValue?.(e.target.value)} required={isRequired}/>
+        <div className={`flex flex-col w-full`}>
+            <label className={`mt-2`}>{label}</label>
+            <input 
+                className={`px-4 py-3 rounded-md border focus:border-blue-500 focus:outline-none focus:bg-indigo-200 mt-2`}
+                type={type} 
+                value={value} 
+                onChange={e => changeValue?.(e.target.value)} 
+                required={isRequired}
+            />
         </div>
     )
 }
